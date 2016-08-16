556e0cc468d211ab11c1ef33536b06c20782f125
Alexis Kyprianou <Alexis.kyprianou@metrobank.plc.uk>
2016-08-16 10:33:12 +0100

    messing with real-time-search

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 import Hello from './hello.jsx';
 import Fruits from './test-ajax.jsx';
-import RUstuff from './test-ru-components.jsx';
+import RealTimeSearch from './real-time-search.jsx';
+//import RUstuff from './test-ru-components.jsx';
