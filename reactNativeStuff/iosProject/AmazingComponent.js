584002f44456a0ecee5386d5de994b0071f4a30a
Alexis Kyprianou <Alexis.kyprianou@metrobank.plc.uk>
2016-08-17 16:54:55 +0100

    added inputfield react-native

--- reactNativeStuff/iosProject/AmazingComponent.js
+++ reactNativeStuff/iosProject/AmazingComponent.js
@@ -7,16 +7,29 @@
 import React, { Component } from 'react';
 import {
   Text,
-  View
+  View,
+  TextInput
 } from 'react-native';
 
 class AmazingComponent extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      text: ''
+    }
+  }
   render() {
     return (
       <View>
         <Text>
           {this.props.title}
         </Text>
+      
+      <TextInput
+        style={{height: 20, width: 100, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(text) => this.setState({text})}
+        value={this.state.text} />
+
       </View>
     );
   }
