750a119c178e31cf924ec52341c29cf6b8273ad3
Alexis Kyprianou <Alexis.kyprianou@metrobank.plc.uk>
2016-08-22 14:32:35 +0100

    add form sample

---
+++ form-sample.jsx
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class FormSample extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			password: '',
+			username: ''
+		}
+
+		this.handleUserChange = this.handleUserChange.bind(this)
+		this.handlePassChange = this.handlePassChange.bind(this)
+		this.handleSubmit = this.handleSubmit.bind(this)
+	}
+
+	handleUserChange(event){
+		this.setState({username: event.target.value})
+	}
+
+	handlePassChange(event){
+		this.setState({password: event.target.value})
+	}
+
+	handleSubmit(event){
+		event.preventDefault();
+		console.log(this.state.username)
+		console.log(this.state.password)
+	}
+
+	render(){
+		return(
+			  <div>
+			  	 <form onSubmit={this.handleSubmit}>
+			  	 		<h1>{this.props.title}</h1>
+			  	 		<input type="text" name="username" onChange={this.handleUserChange} />
+			  	 		<input type="password" name="password" onChange={this.handlePassChange} />
+			  	 		<input type="submit" />
+			  	 </form>
+			  </div>
+			)
+	}
+}
+
+ReactDOM.render(<FormSample title="Form sample" />, document.querySelector('.form-sample'))
--- main.js
+++ main.js
@@ -2,4 +2,5 @@ import Hello from './hello.jsx';
 import Fruits from './test-ajax.jsx';
 import RealTimeSearch from './real-time-search.jsx';
 //import RUstuff from './test-ru-components.jsx';
+import FormSample from './form-sample.jsx';
 import Footer from './footer.jsx';
